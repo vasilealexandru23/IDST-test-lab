35a43448bc63b6b9596effa67f468e751dc4c6d0
Alexandru Gabriel Vasile <vasilealexandru37@gmail.com>
2022-12-15 10:20:28 +0200

    last lab

---
+++ test.py
@@ -0,0 +1,9 @@
+new_letters = { 'B' : 'D',
+                'D' : 'X',
+                'E' : 'V' } 
+               
+
+text_file = open("./text.txt", "r")
+my_string = text_file.read()
+my_string
+text_file.close()
