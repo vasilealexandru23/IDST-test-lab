bee8fd2c11d137ec68ae4fb28db1f05bc6830155
Alexandru Gabriel Vasile <vasilealexandru37@gmail.com>
2022-12-15 10:40:54 +0200

    pastrez doar bot

--- test.py
+++
@@ -1,9 +0,0 @@
-new_letters = { 'B' : 'D',
-                'D' : 'X',
-                'E' : 'V' } 
-               
-
-text_file = open("./text.txt", "r")
-my_string = text_file.read()
-my_string
-text_file.close()
